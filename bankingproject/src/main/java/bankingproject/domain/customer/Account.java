ac0248955c61f5c94466874267de1907524371fb
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-07-18 09:56:42 +0300

    finished working on the account domain logic

--- bankingproject/src/main/java/bankingproject/domain/customer/Account.java
+++ bankingproject/src/main/java/bankingproject/domain/customer/Account.java
@@ -1,5 +1,7 @@
 package bankingproject.domain.customer;
 
+import org.apache.log4j.Logger;
+
 import bankingproject.domain.customer.money.Money;
 
 public class Account {
@@ -7,13 +9,18 @@ public class Account {
 	private int id;
 	private Money balance;
 	
+	private final static Logger logger = Logger.getLogger(Account.class);
+	
 	public Account(int id, Money balance) {
 		this.id = id;
 		this.balance = balance;
 	}
 	
 	public void transfer(Account account, Money amount) {
-		
+		logger.info("Transfer from " + id + " to " + account.id + " amount: " + amount.getAmount() + " " + amount.getCurrency());
+		balance.subtract(amount);
+		account.balance.add(amount);
+		logger.trace("Transaction " + id + "-" + account.id + " : " + amount.getAmount() + " " + amount.getCurrency() + " has been completed");
 	}
 
 	public int getId() {
--- bankingproject/src/test/java/bankingproject/customer/AccountTest.java
+++ bankingproject/src/test/java/bankingproject/customer/AccountTest.java
@@ -1,7 +1,23 @@
 package bankingproject.customer;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import bankingproject.domain.customer.Account;
+import bankingproject.domain.customer.money.Money;
+
 public class AccountTest {
 	
-	
+	@Test
+	public void checkTransaction() {
+		Account aFrom = new Account(102, new Money(500, "USD"));
+		Account aTo = new Account(134, new Money(20, "USD"));
+		
+		aFrom.transfer(aTo, new Money(100, "USD"));
+		
+		assertEquals(400, aFrom.getBalance().getAmount(), 0.01);
+		assertEquals(120, aTo.getBalance().getAmount(), 0.01);
+	}
 	
 }
