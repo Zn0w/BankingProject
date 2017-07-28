05e80f8e85af0f77c515ceae1668a051898b4708
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-07-28 15:23:40 +0300

    implemented transaction domain logic

--- bankingproject/src/main/java/bankingproject/domain/customer/transaction/Transaction.java
+++ bankingproject/src/main/java/bankingproject/domain/customer/transaction/Transaction.java
@@ -2,6 +2,28 @@ package bankingproject.domain.customer.transaction;
 
 public class Transaction {
 	
+	private int accountId;
+	private String info;
 	
+	public Transaction(int accountId, String info) {
+		this.accountId = accountId;
+		this.info = info;
+	}
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
 	
 }
